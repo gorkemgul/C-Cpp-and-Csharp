9fac571b8346d8f66944c7e5c0d1cb4969ed6cd9
Cabbar Gorkem Gul <gorkemgtech@gmail.com>
2022-08-14 21:19:02 +0300

    while loop is done

---
+++ C/Fundamentals of C/whileLoop.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // We're going to cover how to use while loop in c.
+    // let's create an integer.
+    int a;
+    a = 0;
+    while (a < 5)
+    {
+        printf("%d\n", a);
+        a++;
+    }
+}
---
+++ C/Fundamentals of C/while_loop_example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    - We're gonna do an exercise about while loop in c.
+    - Our program is gonna ask us to enter a number and we're gonna enter 100.
+    - The loop is gonna multiply our number with the numbers from 1 to 10.
+    - After the loop is finished, we're gonna get 100, 200, 300, 400, 500, 600, 700, 800, 900 and 1000 as an output.
+    */
+    // let's start by creating an integer
+    int i;
+    // let's take a number from user
+    int number;
+    printf("Please enter a number...\n"); // it's gonna be 100
+    scanf("%d", &number);
+    // let's create a while loop...
+    while (i < 11)
+    {
+        printf("%d\n", i * number);
+        i++;
+    }
+}
