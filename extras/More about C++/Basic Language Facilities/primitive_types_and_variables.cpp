16a5c2dc2d7e192c27a1efc6a999241f271eb5c7
Cabbar Gorkem Gul <gorkemgtech@gmail.com>
2023-02-05 18:13:49 +0300

    add primitive types and variables

---
+++ extras/More about C++/Basic Language Facilities/primitive_types_and_variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    int i;
+    char character = 'g';
+    bool status = true;
+    float f {5.123f};
+    double d {512.342};
+    int arr[5];
+    int arr_[5] {5, 12, 13, 8, 10};
+    
+}
